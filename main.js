86ed884e9b0ed844c1481c5d5a77c004d636c006
amir-reza-programmer <amirrezazare130081@gmail.com>
2022-05-04 21:31:10 +0430

    finish designing popular faq section

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0)
 })
+
+faqDivs = document.querySelectorAll('.faq')
+faqDivs.forEach(faqDiv => {
+    faqDiv.addEventListener('click', (e)=>{
+        console.log(e.target);
+        faqDiv.querySelector('p').classList.toggle('answer__visible')
+    })
+});
